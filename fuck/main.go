e0d7a47675935a080b078ae574892f5f2694805f
Oleg Yamnikov <bgeyts667@gmail.com>
2016-01-15 23:57:12 +0500

    Refactor code

--- fuck/main.go
+++ fuck/main.go
@@ -8,73 +8,17 @@ import (
 	"github.com/mitchellh/go-ps"
 )
 
-const Usage = "Usage: fuck [you] <process-name>"
-
-var (
-	Chars   = []rune(" -_.abcdefghijklmnopqrstuvwxyz1234567890")
-	Flipped = []rune(" -_'ɐqɔpǝɟɓɥıɾʞlɯuodbɹsʇnʌʍxʎz⇂zƐㄣϛ9ㄥ860")
-)
-
-var ProcessName string
-
-func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		Fatal(Usage)
-	}
-	ProcessName = os.Args[len(os.Args)-1]
-	if ProcessName == "you" {
-		Fatal(Usage)
-	}
-	pid, err := FindProcess(ProcessName)
-	if err != nil {
-		Fatal(err)
-	}
-	err = KillerProcess(pid)
-	if err != nil {
-		Fatal(err)
-	}
-	RageFace(ProcessName)
-	ShokFace()
-}
-
-func FindProcess(pattern string) (pid int, err error) {
-	processes, err := ps.Processes()
-	if err != nil {
-		return 0, err
-	}
-
-	for _, p := range processes {
-		if pattern == p.Executable() && os.Getpid() != p.Pid() {
-			return p.Pid(), nil
-		}
-	}
-	return 0, fmt.Errorf("Process not found")
-}
-
-func KillerProcess(pid int) (err error) {
-	osproc, err := os.FindProcess(pid)
-	if err != nil {
-		return err
-	}
-	osproc.Kill()
-	return nil
-}
-
-func Fatal(err interface{}) {
-	fmt.Println(err)
-	os.Exit(1)
-}
-
 func RageFace(args ...interface{}) {
 	fmt.Println()
 	fmt.Print("  (╯°□°）╯︵")
 	fmt.Println(Flip(fmt.Sprint(args...)))
 	fmt.Println()
 }
-func ShokFace(args ...interface{}) {
+
+func ShockFace(args ...interface{}) {
 	fmt.Println()
 	fmt.Print("  (；￣Д￣) . o O( ")
-	fmt.Print(args...)
+	fmt.Print("It didn't work out as expected...")
 	fmt.Println(" )")
 	fmt.Println()
 }
@@ -95,6 +39,11 @@ func ReverseRunes(r []rune) {
 	}
 }
 
+var (
+	Chars   = []rune(" -_.abcdefghijklmnopqrstuvwxyz1234567890")
+	Flipped = []rune(" -_'ɐqɔpǝɟɓɥıɾʞlɯuodbɹsʇnʌʍxʎz⇂zƐㄣϛ9ㄥ860")
+)
+
 func Flip(str string) string {
 	str = strings.ToLower(str)
 
@@ -110,4 +59,66 @@ func Flip(str string) string {
 	ReverseRunes(buf)
 
 	return string(buf)
+}
+
+func ParseArgs() (pname string, ok bool) {
+	if len(os.Args) != 2 && len(os.Args) != 3 {
+		return "", false
+	}
+
+	pname = os.Args[len(os.Args)-1]
+	if pname == "you" {
+		return "", false
+	}
+
+	return pname, true
+}
+
+func FindProcess(pattern string) (pid int, err error) {
+	processes, err := ps.Processes()
+	if err != nil {
+		return 0, err
+	}
+
+	for _, p := range processes {
+		if p.Pid() == os.Getpid() {
+			continue
+		}
+		if p.Executable() != pattern {
+			continue
+		}
+		return p.Pid(), nil
+	}
+
+	return 0, fmt.Errorf("Process not found")
+}
+
+func FindAndKill(pname string) error {
+	pid, err := FindProcess(pname)
+	if err != nil {
+		return err
+	}
+
+	osproc, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	osproc.Kill()
+
+	return nil
+}
+
+func main() {
+	pname, ok := ParseArgs()
+	if !ok {
+		fmt.Println("Usage: fuck [you] <process-name>")
+		os.Exit(1)
+	}
+
+	if err := FindAndKill(pname); err != nil {
+		ShockFace()
+		os.Exit(1)
+	}
+
+	RageFace(pname)
 }
